c75e65ff7b074a8336e3fa4afee760eb4a619640
Gshisan <425139963@qq.com>
2018-10-09 17:56:24 +0800

    feat: todo

--- src/index.js
+++ src/index.js
@@ -1,19 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
-import store, {doAdd, doReduce, doAddAsync} from './index.redux'
 
-function render() {
-  ReactDOM.render(
-    <App
-      store={store}
-      doAdd={doAdd}
-      doReduce={doReduce}
-      doAddAsync={doAddAsync}
-    />,
-    document.getElementById('root')
-  )
-}
-render()
-store.subscribe(render)
+import { Provider } from 'react-redux'
+import store from './redux/store'
+import TodoApp from './todoApp'
+
+ReactDOM.render(
+  <Provider store={store}>
+    <TodoApp />
+  </Provider>,
+  document.getElementById('root')
+)
+// import App from './App'
+// import store, {doAdd, doReduce, doAddAsync} from './index.redux'
+
+// function render() {
+//   ReactDOM.render(
+//     <App
+//       store={store}
+//       doAdd={doAdd}
+//       doReduce={doReduce}
+//       doAddAsync={doAddAsync}
+//     />,
+//     document.getElementById('root')
+//   )
+// }
+// render()
+// store.subscribe(render)
 
---
+++ src/todoApp.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import AddTodo from './components/AddTodo'
+import TodoList from './components/TodoList'
+import VisibilityFilters from './components/VisibilityFilters'
+
+export default function TodoApp() {
+  return (
+    <div>
+      <h1> todo List</h1>
+      <AddTodo />
+      <TodoList />
+      <VisibilityFilters />
+    </div>
+  )
+}
