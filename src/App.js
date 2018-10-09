decd06dc0e9ee66bb0133a8dab06043790874dc7
Gshisan <425139963@qq.com>
2018-10-09 10:48:22 +0800

    feat: integrated redux to react

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Button } from 'antd-mobile'
+import React from 'react'
 
-
-class App extends Component {
+class App extends React.Component {
   render() {
+    const { store, doAdd, doReduce, doAddAsync } = {...this.props}
     return (
-      <Button className="primary">按钮</Button>
+      <div>
+        hello redux 现在是{store.getState()}
+        <button onClick={() => { store.dispatch(doAdd()) }}>加1</button>
+        <button onClick={() => { store.dispatch(doReduce()) }}>减1</button>
+        <button onClick={() => { store.dispatch(doAddAsync()) }}>延时加1</button>
+      </div>
     )
   }
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import store, {doAdd, doReduce, doAddAsync} from './index.redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function render() {
+  ReactDOM.render(
+    <App
+      store={store}
+      doAdd={doAdd}
+      doReduce={doReduce}
+      doAddAsync={doAddAsync}
+    />,
+    document.getElementById('root')
+  )
+}
+render()
+store.subscribe(render)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
