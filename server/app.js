425058d9e52dc7affb0115ef1563d3f3e1c7e178
soulSingerK <425139963@qq.com>
2018-10-21 13:38:38 +0800

    feat: add connect rear end and front end logic

--- server/app.js
+++ server/app.js
@@ -1,14 +1,20 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser') 
-const userRouter = require('./user') 
+const userRouter = require('./user')
 const app = express()
+
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+io.on('connection', () => {
+  console.log('be connect')
+})
 
 app.use(cookieParser())
 app.use(bodyParser.json())
 
 app.use('/user', userRouter)
 
-app.listen(1313, () => {
+server.listen(1313, () => {
   console.log(`run on the port 1313`)
 })
--- src/component/chat/chat.js
+++ src/component/chat/chat.js
@@ -1,6 +1,11 @@
 import React from 'react'
+import io from 'socket.io-client'
 
 class Chat extends React.Component {
+
+  componentDidMount() {
+    io('ws://localhost:1313')
+  }
 
   render() {
     return (
